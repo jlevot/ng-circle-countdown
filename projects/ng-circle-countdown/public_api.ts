d2b49b3028a36fdfd1b66e1f88e01ca207d74717
jean-baptiste.levot <jean-baptiste.levot@devmachine.fr>
2024-07-01 09:40:47 +0200

    Remove unused module

--- projects/ng-circle-countdown/public_api.ts
+++ projects/ng-circle-countdown/public_api.ts
@@ -1,4 +1,3 @@
 export * from './src/component/ng-circle-countdown.component';
-export * from './src/component/ng-circle-countdown.module';
 export * from './src/model/rotation-type';
 export * from './src/services/countdown.service'
