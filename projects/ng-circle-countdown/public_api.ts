bed5030bc10870c0e89bf04747f9f8c82d5e29f2
jean-baptiste.levot <jean-baptiste.levot@devmachine.fr>
2025-02-06 08:53:01 -0800

    Add countdown model to public API

--- projects/ng-circle-countdown/public_api.ts
+++ projects/ng-circle-countdown/public_api.ts
@@ -1,3 +1,4 @@
 export * from './src/component/ng-circle-countdown.component';
 export * from './src/model/rotation-type';
+export * from './src/model/countdown';
 export * from './src/services/countdown.service'
